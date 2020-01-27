80042c54f424906672cf90ff09441128c3def33b
Kunwar Sahni <kunwar.sahni01@gmail.com>
2020-01-27 18:50:04 -0500

    Added Detail View Controller + Tab Bar

---
+++ Flix/MovieGridViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MovieGridViewController.swift
+//  Flix
+//
+//  Created by Kunwar Sahni on 1/27/20.
+//  Copyright © 2020 purdue. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -60,5 +60,16 @@ class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDa
         return cell
         //Testing Source Control From Xcode
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 
 }

a4f99d29acdd714573f2309431c7088d23f55897
Kunwar Sahni <kunwar.sahni01@gmail.com>
2020-02-02 13:00:21 -0500

    Added Collection View

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Kunwar Sahni on 2/2/20.
+//  Copyright © 2020 purdue. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
